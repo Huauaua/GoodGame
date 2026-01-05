c1eea484c6c321a347f0b7c0439be6ce51dd8bc0
Huauaua <1775778681@qq.com>
2026-01-05 20:26:49 +0800

    鼠标hover练习

---
+++ scripts/HoverColorRect.cs
@@ -0,0 +1,34 @@
+﻿using Godot;
+
+public partial class HoverColorRect : ColorRect
+{
+    [Export] private Color normalColor = new Color(0.2f, 0.2f, 0.2f, 1);
+    [Export] private Color hoverColor = new Color(0.4f, 0.6f, 0.8f, 1);
+    private Label Label;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        Label = GetNode<Label>("Label");
+        Color = normalColor;
+
+        // 启用鼠标检测
+        MouseFilter = Control.MouseFilterEnum.Pass;
+
+        // 连接信号
+        MouseEntered += OnMouseEntered;
+        MouseExited += OnMouseExited;
+    }
+
+    private void OnMouseEntered()
+    {
+        Color = hoverColor;
+        Label.Text = new string("感觉有人在我上面");
+    }
+
+    private void OnMouseExited()
+    {
+        Color = normalColor;
+        Label.Text = new string("我是个Label");
+    }
+}
--- scripts/Player.cs
+++ scripts/Player.cs
@@ -1,11 +1,13 @@
 ﻿using Godot;
 using System;
+using System.Collections.Specialized;
 
 public partial class Player : CharacterBody2D
 {
     private NodePath inputComponentPath = "InputComponent";
 	private InputComponent _inputComponent = new();
-    private const float SPEED = 300.0f;
+    private const float SPEED = 500.0f;
+	private const float acc = 0.95f;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -16,7 +18,19 @@ public partial class Player : CharacterBody2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _PhysicsProcess(double delta)
 	{
-		Velocity = _inputComponent.Direction * SPEED;
-		MoveAndSlide();
+		if ((long)(Velocity.X * Velocity.X + Velocity.Y * Velocity.Y) == 0 && _inputComponent.Direction != Vector2.Zero)
+        {
+            Velocity = _inputComponent.Direction * SPEED;
+        }
+        else
+        {
+            float newX = Velocity.X * acc;
+            float newY = Velocity.Y * acc;
+            if (Math.Abs(newX) < 100) newX = 0;
+            if (Math.Abs(newY) < 100) newY = 0;
+            Velocity = new(newX, newY);
+        }
+
+        MoveAndSlide();
 	}
 }
